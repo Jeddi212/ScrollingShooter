984be1c255e867da78d9bf00ec2baee732e52d45
Jeddi <jedediahf51@gmail.com>
2022-03-06 11:34:34 +0700

    first commit

---
+++ conf.lua
@@ -0,0 +1,11 @@
+-- Configuration
+-- http://www.osmstudios.com/tutorials/your-first-love2d-game-in-200-lines-part-1-of-3
+function love.conf(t)
+    t.title = "Scrolling Shooter Tutorial" -- The title of the window the game is in (string)
+	t.version = "11.4"         -- The LÖVE version this game was made for (string)
+	t.window.width = 480        -- we want our game to be long and thin.
+	t.window.height = 800
+
+	-- For Windows debugging
+	t.console = true -- Change to false for releasing game
+end
---
+++ main.lua
@@ -0,0 +1,21 @@
+debug = true -- Set false for realease
+
+--[[
+    love.load is called when the game first starts. 
+    We'll load our assets -- images, sounds, etc -- here
+]]--
+function love.load(arg)
+    
+end
+
+--[[
+    love.update and love.draw 
+    are called on every frame.
+]]--
+function love.update(dt)
+    
+end
+
+function love.draw(dt)
+    
+end
