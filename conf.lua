78a1eca224da46624ef0001097b163b52b68d90a
Jeddi <jedediahf51@gmail.com>
2022-03-06 12:08:53 +0700

    Add player plane.png ✈

--- conf.lua
+++ conf.lua
@@ -4,7 +4,7 @@ function love.conf(t)
     t.title = "Scrolling Shooter Tutorial" -- The title of the window the game is in (string)
 	t.version = "11.4"         -- The LÖVE version this game was made for (string)
 	t.window.width = 480        -- we want our game to be long and thin.
-	t.window.height = 800
+	t.window.height = 710
 
 	-- For Windows debugging
 	t.console = true -- Change to false for releasing game
--- main.lua
+++ main.lua
@@ -5,17 +5,20 @@ debug = true -- Set false for realease
     We'll load our assets -- images, sounds, etc -- here
 ]]--
 function love.load(arg)
-    
+    playerImg = love.graphics.newImage('assets/plane.png')
+    --we now have an asset ready to be used inside Love
 end
 
 --[[
     love.update and love.draw 
     are called on every frame.
 ]]--
+
+playerImg = nil -- This just for storage
 function love.update(dt)
     
 end
 
 function love.draw(dt)
-    
+    love.graphics.draw(playerImg, 100, 100)
 end
