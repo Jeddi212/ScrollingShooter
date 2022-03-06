a42dde79405614a1d212bc2ce5e0280dd76a1f8e
Jeddi <jedediahf51@gmail.com>
2022-03-06 12:25:22 +0700

    Player move <-  ->

--- main.lua
+++ main.lua
@@ -13,12 +13,21 @@ end
     love.update and love.draw 
     are called on every frame.
 ]]--
-
-player = { x = 200, y = 630, speed = 150, img = nil }
 function love.update(dt)
-    
+    -- I always start with an easy way to exit the game
+    if love.keyboard.isDown('escape') then
+        love.event.push('quit')
+    end
+
+    if love.keyboard.isDown("left") then
+        player.x = player.x - (player.speed*dt)
+    elseif love.keyboard.isDown('right') then
+        player.x = player.x + (player.speed*dt)
+    end
 end
 
+player = { x = 185, y = 610, speed = 150, img = nil }
+
 function love.draw(dt)
-    love.graphics.draw(player.img, 100, 100)
+    love.graphics.draw(player.img, player.x, player.y)
 end
